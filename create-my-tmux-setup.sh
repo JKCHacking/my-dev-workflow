fc5521471edb47a74cefcc0eed779df41a406699
Joshnee Kim Cunanan <joshnee.kim.cunanan@gmail.com>
2025-07-26 14:56:49 +0800

    updated tmux configuration to have desired windows and panes

--- create-my-tmux-setup.sh
+++ create-my-tmux-setup.sh
@@ -4,19 +4,26 @@
 SESSION_NAME=$1
 PROJECT_DIR=$2
 
-if [ -z "$SESSION_NAME" ] || [ -z "$PROJECT_DIR" ]; then
-  echo "Usage: $0 <session-name> <project-dir>"
-  exit 1
+tmux has-session -t $SESSION 2>/dev/null
+
+if [ $? != 0 ]; then
+    if [ -z "$SESSION_NAME" ] || [ -z "$PROJECT_DIR" ]; then
+      echo "Usage: $0 <session-name> <project-dir>"
+      exit 1
+    fi
+
+    # Start new session and create first window with Neovim
+    tmux new-session -d -s "$SESSION_NAME" -n "nvim"
+    tmux send-keys -t $SESSION_NAME:"nvim" "cd $PROJECT_DIR && nvim" C-m
+    # Split the window vertically for terminal
+    tmux split-window -h -t $SESSION_NAME:"nvim"
+    tmux send-keys -t $SESSION_NAME:"nvim.1" "cd $PROJECT_DIR && clear" C-m
+
+    # Create second window with lazygit
+    tmux new-window -t "$SESSION_NAME" -n "git"
+    tmux send-keys -t $SESSION_NAME:"git" "cd $PROJECT_DIR && lazygit" C-m
 fi
 
-# Start new session and create first window with Neovim
-tmux new-session -d -s "$SESSION_NAME" -c "$PROJECT_DIR" -n "dev" "nvim"
-
-# Split the window horizontally for terminal
-tmux split-window -h -t "$SESSION_NAME:0" -c "$PROJECT_DIR"
-
-# Create second window with lazygit
-tmux new-window -t "$SESSION_NAME" -n "git" -c "$PROJECT_DIR" "lazygit"
 
 # Attach to session
 tmux attach -t "$SESSION_NAME"
